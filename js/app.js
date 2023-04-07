8ac14b230677dc02c56872965d029f97b283b196
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2023-04-07 15:49:23 +0500

    slider-theend

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+  });
